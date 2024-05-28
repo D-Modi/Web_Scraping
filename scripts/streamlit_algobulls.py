14e04536b0c838865dd7b7e5a6faec9d981ff219
D-Modi <126412094+D-Modi@users.noreply.github.com>
2024-05-28 12:41:36 +0530

    Formated the website

--- scripts/streamlit_algobulls.py
+++ scripts/streamlit_algobulls.py
@@ -56,24 +56,23 @@ def daisply(daily_returns, Quant):
         st.header("Daily Analysis")
     else:
         st.header(f"{Quant}ly Analysis")
-        st.write(f"{Quant}ly Average Returns: {Alanyze.avgReturns(daily_returns)[0]}")
-        st.write(f"{Quant}ly Average Returns %: {Alanyze.avgReturns(daily_returns)[1]}%")
+        st.write(f"***{Quant}ly Average Returns***: {Alanyze.avgReturns(daily_returns)[0]}")
+        st.write(f"***{Quant}ly Average Returns %***: {Alanyze.avgReturns(daily_returns)[1]}%")
     
     days_hist, days_tab = Alanyze.compare_hist(daily_returns, [1000, 2000, 3000, 4000, 5000], Quant)
     st.subheader(f"Number of {Quant} of profit/loss above a threshold") 
     st.pyplot(days_hist)
-    
     with st.expander("More information"):
-        st.write(f"Number of trading {Quant}s: {Alanyze.trading_num(daily_returns)}")
-        st.write(f"Number of Profitable {Quant}s: {Alanyze.num_profit(daily_returns)} {Quant}")
-        st.write(f"Number of Loss Making {Quant}s: {Alanyze.num_loss(daily_returns)} {Quant} ")
-        st.write(f"Most Profitable {Quant}: {Alanyze.max_profit(daily_returns)[1]}")
-        st.write(f"Maximum Gains in a {Quant}: {Alanyze.max_profit(daily_returns)[0]}")
-        st.write(f"Least Profitable {Quant}: {Alanyze.min_profit(daily_returns)[1]}")
-        st.write(f"Maximum loss in a {Quant}: {Alanyze.min_profit(daily_returns)[0]}")
+        st.write(f"Number of ***trading {Quant}s***: {Alanyze.trading_num(daily_returns)}")
+        st.write(f"Number of ***Profitable {Quant}s***: {Alanyze.num_profit(daily_returns)} {Quant}")
+        st.write(f"Number of ***Loss Making {Quant}s***: {Alanyze.num_loss(daily_returns)} {Quant} ")
+        st.write(f"***Most Profitable {Quant}***: {Alanyze.max_profit(daily_returns)[1]}")
+        st.write(f"Maximum ***Gains*** in a {Quant}: {Alanyze.max_profit(daily_returns)[0]}")
+        st.write(f"***Least Profitable {Quant}***: {Alanyze.min_profit(daily_returns)[1]}")
+        st.write(f"Maximum ***Loss*** in a {Quant}: {Alanyze.min_profit(daily_returns)[0]}")
 
     st.subheader(f"Profit/Loss Data per {Quant}")
-    st.bar_chart(daily_returns, y=['pnl_absolute'])
+    st.bar_chart(daily_returns, y=['pnl_absolute'] )
     if 'cum_pnl' in daily_returns.columns:
         st.subheader("Cumulative Profit and loss")
         st.line_chart(daily_returns, y=['cum_pnl'])
@@ -83,8 +82,8 @@ def daisply(daily_returns, Quant):
 def display(weekday_returns):
     st.subheader(f"Profit/Loss Data per Day of Week")
     st.bar_chart(weekday_returns, y=['pnl_absolute'] )
-    st.write(f"Most Profitable Day of the week: {Alanyze.max_profit(weekday_returns)[1]}")
-    st.write(f"Least Profitable Day of the week: {Alanyze.min_profit(weekday_returns)[1]}")
+    st.write(f"***Most Profitable Day*** of the week: {Alanyze.max_profit(weekday_returns)[1]}")
+    st.write(f"***Least Profitable Day*** of the week: {Alanyze.min_profit(weekday_returns)[1]}")
     tab = weekday_returns['pnl_absolute']
     st.table(tab)
 
@@ -92,64 +91,72 @@ csv = f"../files/StrategyBacktestingPLBook-{option}.csv"
 Alanyze = StatergyAnalysis(csv)
 daily_returns, monthly_returns, weekday_returns, weekly_returns, yearly_returns = Alanyze.analysis()
 
-st.write(f"Max Drawdown: {Alanyze.drawdown_max}")
-st.write(f"Maximum Drawdowm percentage: {Alanyze.drawdown_pct}")
+st.write(f"***Max Drawdown***: {Alanyze.drawdown_max}")
+st.write(f"***Maximum Drawdowm percentage***: {Alanyze.drawdown_pct}")
+st.subheader("Drawdown Curve")
 st.line_chart(Alanyze.csv_data, y='drawdown_pct', x='Day')
-st.write(f"Average loss per losing trade: {Alanyze.winCount(Alanyze.csv_data, -1)}")
-st.write(f"Average gain per winning trade: {Alanyze.winCount(Alanyze.csv_data, 1)}")
-st.write(f"Maximum Gains: {Alanyze.max_profit(Alanyze.csv_data)[0]}")
-st.write(f"Minimum Gain: {Alanyze.min_profit(Alanyze.csv_data)[0]}")
-st.write(f"Number of short trades: {Alanyze.num_tradeType('short')}")
-st.write(f"Number of long trades: {Alanyze.num_tradeType('long')}")
-st.write (f"Average Trades per Day: {Alanyze.avgTrades(daily_returns)}")
-st.write(f"Number of wins: {Alanyze.num_profit(Alanyze.csv_data)}")
-st.write(f"Number of losses: {Alanyze.num_loss(Alanyze.csv_data)}")
-st.write(f"HIT Ratio: {Alanyze.HIT()}")
-st.write(f"ROI: {Alanyze.roi(monthly_returns)[0]}")
-st.write(f"ROI %: {Alanyze.roi(monthly_returns)[1]}%")
-st.write(f"Profit Factor: {Alanyze.ProfitFactor()}")
-st.write(f"Yearly Volatility: {Alanyze.annual_std}")
-st.write(f"Max Win Streak: {Alanyze.max_consecutive(1)}")
-st.write(f"Max Loss streak: {Alanyze.max_consecutive(-1)}")
-
+st.write(f"***Average loss per losing trade***: {Alanyze.winCount(Alanyze.csv_data, -1)}")
+st.write(f"***Average gain per winning trade***: {Alanyze.winCount(Alanyze.csv_data, 1)}")
+st.write(f"***Maximum Gains***: {Alanyze.max_profit(Alanyze.csv_data)[0]}")
+st.write(f"***Minimum Gains***: {Alanyze.min_profit(Alanyze.csv_data)[0]}")
+st.write(f"Number of ***short trades***: {Alanyze.num_tradeType('short')}")
+st.write(f"Number of ***long trades***: {Alanyze.num_tradeType('long')}")
+st.write (f"***Average Trades per Day***: {Alanyze.avgTrades(daily_returns)}")
+st.write(f"Number of ***wins***: {Alanyze.num_profit(Alanyze.csv_data)}")
+st.write(f"Number of ***losses***: {Alanyze.num_loss(Alanyze.csv_data)}")
+st.write(f"***HIT Ratio***: {Alanyze.HIT()}")
+st.write(f"***ROI***: {Alanyze.roi(monthly_returns)[0]}")
+st.write(f"***ROI %***: {Alanyze.roi(monthly_returns)[1]}%")
+st.write(f"***Profit Factor***: {Alanyze.ProfitFactor()}")
+st.write(f"***Yearly Volatility***: {Alanyze.annual_std}")
+st.write(f"***Max Win Streak***: {Alanyze.max_consecutive(1)}")
+st.write(f"***Max Loss streak***: {Alanyze.max_consecutive(-1)}")
 if month:
     last_month = monthly_returns.index[-1]
     last_month_data = Alanyze.csv_data[Alanyze.csv_data['Month'] == last_month]
-    st.write(f"Win Rate for last Month: {Alanyze.win_rate(last_month_data)}")
+    st.write(f"Win Rate for ***last Month***: {Alanyze.win_rate(last_month_data)}")
 if week:
     last_month = weekly_returns.index[-1]
     last_month_data = Alanyze.csv_data[Alanyze.csv_data['Week'] == last_month]
-    st.write(f"Win Rate for last Week: {Alanyze.win_rate(last_month_data)}")
+    st.write(f"Win Rate for ***last Week***: {Alanyze.win_rate(last_month_data)}")
 if year:
     last_month = yearly_returns.index[-1]
     last_month_data = Alanyze.csv_data[Alanyze.csv_data['Year'] == last_month]
-    st.write(f"Win Rate for last Year: {Alanyze.win_rate(last_month_data)}")
+    st.write(f"Win Rate for ***last Year***: {Alanyze.win_rate(last_month_data)}")
 if months6:
     last_month = np.array(monthly_returns.index[-6:])
     last_month_data = Alanyze.csv_data[Alanyze.csv_data['Month'].isin(last_month)]
-    st.write(f"Win Rate for last 6 Months: {Alanyze.win_rate(last_month_data)}")
+    st.write(f"Win Rate for ***last 6 Months***: {Alanyze.win_rate(last_month_data)}")
 if quater:
     last_month = np.array(monthly_returns.index[-4:])
     last_month_data = Alanyze.csv_data[Alanyze.csv_data['Month'].isin(last_month)]
-    st.write(f"Win Rate for last Quater: {Alanyze.win_rate(last_month_data)}")
+    st.write(f"Win Rate for ***last Quater:*** {Alanyze.win_rate(last_month_data)}")
 
-st.write(f"Sharpe Ratio {Alanyze.Sharpe()}")
-st.write(f"Calmar Ratio {Alanyze.Calmar()}")
-st.write(f"Sortino Ratio {Alanyze.Sortino()}")
+st.write(f"***Sharpe Ratio:*** {Alanyze.Sharpe()}")
+st.write(f"***Calmar Ratio:*** {Alanyze.Calmar()}")
+st.write(f"***Sortino Ratio:*** {Alanyze.Sortino()}")
 
+st.subheader("Equity Curve")
 st.line_chart(Alanyze.csv_data, y='equity_curve', x='Day')
 if threeD:
-    st.write(f"Returns for the last 3 Days: {Alanyze.Treturns(3)[1]}%")
+    d = relativedelta(days=3)
+    st.write(f"Returns for the ***last 3 Days***: {Alanyze.Treturns(3)[1]}%")
 if thirtyD:
-    st.write(f"Returns for the last 30 Days: {Alanyze.Treturns(30)[1]}%")
+    d = relativedelta(days=30)
+    st.write(f"Returns for the ***last 30 Days***: {Alanyze.Treturns(30)[1]}%")
 if twoW:
-    st.write(f"Returns for the last 2 Weeks: {Alanyze.Treturns(14)[1]}%")
+    d = relativedelta(days=14)
+    st.write(f"Returns for the ***last 2 Weeks***: {Alanyze.Treturns(14)[1]}%")
 if sixM:
-    st.write(f"Returns for the last 6 Months: {Alanyze.Treturns(180)[1]}%")
+    d = relativedelta(months=6)
+    st.write(f"Returns for the ***last 6 Months***: {Alanyze.Treturns(180)[1]}%")
 if oneY:
-    st.write(f"Returns for the last 1 Year: {Alanyze.Treturns(365)[1]}%")
+    d = relativedelta(years=1)
+    st.write(f"Returns for the ***last 1 Year***: {Alanyze.Treturns(365)[1]}%")
 if twoY:
-    st.write(f"Returns for the last 2 Years: {Alanyze.Treturns(365*2)[1]}%")
+    d = relativedelta(years=2)
+    st.write(f"Returns for the ***last 2 Years***: {Alanyze.Treturns(365*2)[1]}%")
+
 
 if Daily:
     daisply(daily_returns, "Day")
@@ -161,6 +168,8 @@ if Weekly:
     daisply(weekly_returns, "Week")
 if Day:
     display(weekday_returns)
+
+
 
 
 
