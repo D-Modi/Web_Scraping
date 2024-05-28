16c85dae9e6ef801083233061d21d5a7c2fdd979
D-Modi <126412094+D-Modi@users.noreply.github.com>
2024-05-28 13:05:56 +0530

    Added monthly ROI calculation

--- scripts/stratrgy_analysis.py
+++ scripts/stratrgy_analysis.py
@@ -8,12 +8,12 @@ from dateutil.relativedelta import relativedelta
 class StatergyAnalysis:
 
     def __init__(self, csv_filepath):
-        
         self.csv_data = self.get_csv(csv_filepath)
         self.daily_returnts = None
         self.equity_curve_value = self.csv_data['equity_curve'].tolist()
         self.risk_free_rate = 0.02
-        self.initial_investment = self.equity_curve_value[-1]
+        #self.initial_investment = self.equity_curve_value[-1]
+        self.initial_investment = 150000
         self.equity_PctChange = None
         self.annual_std = 0
         self.annual_mean = 0
@@ -38,7 +38,6 @@ class StatergyAnalysis:
             data['Month'] = data['Month'].dt.strftime('%Y-%m')
             data['Year'] = data['Year'].dt.strftime('%Y')
             data['weekday'] = data['weekday'].dt.strftime('%a')
-            
         except Exception as e:
             data = pd.read_csv(filepath, parse_dates=['EN_TIME'])
 
@@ -83,7 +82,8 @@ class StatergyAnalysis:
 
         Monthly_returns = self.csv_data.groupby('Month').sum(numeric_only = True)
         Monthly_returns['cum_pnl'] = Monthly_returns['pnl_absolute'].cumsum()
-        monthly_analysis = Monthly_returns[['pnl_absolute', 'cum_pnl']]
+        Monthly_returns['roi'] = round((Monthly_returns['cum_pnl']/self.initial_investment)*100,2)
+        monthly_analysis = Monthly_returns[['pnl_absolute', 'cum_pnl', 'roi']]
 
         weekday_returns = self.csv_data.groupby('weekday').sum(numeric_only = True)
         weekday_returns['cum_pnl'] = weekday_returns['pnl_absolute'].cumsum()
@@ -129,6 +129,7 @@ class StatergyAnalysis:
         return sortino_ratio
     
     def max_consecutive(self, quant):
+  
         if quant ==1:
             positive_mask = self.csv_data['pnl_absolute'] > 0
         else:
@@ -262,3 +263,4 @@ class StatergyAnalysis:
         profit = positive_values['pnl_absolute'].sum()
         loss = neg_values['pnl_absolute'].sum()
         return profit/loss
+    
