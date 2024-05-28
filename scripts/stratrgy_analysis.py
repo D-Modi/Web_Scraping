fe9d0a0734ed44c3b03f174d0d0c6211c5f63232
D-Modi <126412094+D-Modi@users.noreply.github.com>
2024-05-28 12:29:16 +0530

    Additional Functions added

--- scripts/stratrgy_analysis.py
+++ scripts/stratrgy_analysis.py
@@ -2,11 +2,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt  
 import datetime as dt
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 class StatergyAnalysis:
 
     def __init__(self, csv_filepath):
+        
         self.csv_data = self.get_csv(csv_filepath)
-
+        self.daily_returnts = None
+        self.equity_curve_value = self.csv_data['equity_curve'].tolist()
+        self.risk_free_rate = 0.02
+        self.initial_investment = self.equity_curve_value[-1]
+        self.equity_PctChange = None
+        self.annual_std = 0
+        self.annual_mean = 0
+        self.drawdown_max, self.drawdown_pct = self.drawdown()
+        self.daily_equity_Curve()
+        self.num_wins = self.num_profit(self.csv_data)
+        self.numTrades = len(self.csv_data)
 
     def get_csv(self, filepath):
 
@@ -24,6 +38,7 @@ class StatergyAnalysis:
             data['Month'] = data['Month'].dt.strftime('%Y-%m')
             data['Year'] = data['Year'].dt.strftime('%Y')
             data['weekday'] = data['weekday'].dt.strftime('%a')
+            
         except Exception as e:
             data = pd.read_csv(filepath, parse_dates=['EN_TIME'])
 
@@ -41,13 +56,30 @@ class StatergyAnalysis:
         
         if 'P&L' in data.columns:
             data.rename(columns={'P&L': 'pnl_absolute'}, inplace=True)
+        
+        if 'Equity Curve' in data.columns:
+            data.rename(columns={'Equity Curve': 'equity_curve'}, inplace=True)
 
+        if 'EN_TT' in data.columns:
+            data.rename(columns={'EN_TT': 'entry_transaction_type'}, inplace=True)
+        
         return data
+    
+    def daily_equity_Curve(self):
+        daily_equity_curve = self.csv_data.groupby('Day')['equity_curve'].last()
+        self.equity_PctChange = daily_equity_curve.pct_change().dropna()
+        self.annual_mean = self.equity_PctChange.mean() * 252   
+        self.annual_std = self.equity_PctChange.std() * 252   
+    
+    def get_last_equity_curve(self, group):
+        return group['equity_curve'].iloc[0]
 
     def analysis(self):
         daily_returns = self.csv_data.groupby('Day').sum(numeric_only = True)
+        daily_returns['equity_curve'] = self.csv_data.groupby('Day').apply(self.get_last_equity_curve)
         daily_returns['cum_pnl'] = daily_returns['pnl_absolute'].cumsum()
-        daily_analysis = daily_returns[['pnl_absolute', 'cum_pnl']]
+        daily_analysis = daily_returns[['pnl_absolute', 'cum_pnl', 'equity_curve']]
+        self.daily_returnts = daily_analysis
 
         Monthly_returns = self.csv_data.groupby('Month').sum(numeric_only = True)
         Monthly_returns['cum_pnl'] = Monthly_returns['pnl_absolute'].cumsum()
@@ -62,19 +94,95 @@ class StatergyAnalysis:
         weekly_returns[['pnl_absolute','cum_pnl']]
 
         yearly_returns = self.csv_data.groupby('Year').sum(numeric_only = True)
-        yearly_returns['pnl_absolute']
+        yearly_returns['cum_pnl'] = yearly_returns['pnl_absolute'].cumsum()
+        yearly_returns['equity_curve'] = self.csv_data.groupby('Year').apply(self.get_last_equity_curve)
+        yearly_returns[['pnl_absolute', 'cum_pnl']]
 
         return daily_analysis, monthly_analysis, weekday_returns, weekly_returns, yearly_returns 
 
     def max_profit(self, returns):
-        max_profitable_day = returns['pnl_absolute'].max()
-        return max_profitable_day
+        max_profits = returns['pnl_absolute'].max()
+        max_profitable_day = returns['pnl_absolute'].idxmax()
+        maxi = [max_profits, max_profitable_day]
+        return maxi
 
     def min_profit(self, returns):
         min_profitable_day = returns['pnl_absolute'].min()
-        return min_profitable_day 
+        min_profit_day =  returns['pnl_absolute'].idxmin()
+        return [min_profitable_day, min_profit_day] 
+    
+    def Sharpe(self):
+        sharpe_ratio = (self.annual_mean - self.risk_free_rate) / self.annual_std
+        print(sharpe_ratio)
+        return sharpe_ratio
+    
+    def Calmar(self):
+        calmar_ratio = self.annual_mean / self.drawdown_pct
+        print(calmar_ratio)
+        return calmar_ratio
+    
+    def Sortino(self):
+        downside_returns = self.equity_PctChange[self.equity_PctChange < 0]
+        downside_deviation = downside_returns.std() * np.sqrt(252)
+        sortino_ratio = (self.annual_mean - self.risk_free_rate) / downside_deviation
+        print(sortino_ratio)
+        return sortino_ratio
+    
+    def max_consecutive(self, quant):
+        if quant ==1:
+            positive_mask = self.csv_data['pnl_absolute'] > 0
+        else:
+            positive_mask = self.csv_data['pnl_absolute']  < 0
+        
+        grouped = (positive_mask != positive_mask.shift()).cumsum()
+        positive_counts = positive_mask.groupby(grouped).cumsum()
+        return positive_counts.max()
+    
+    def win_rate(self, daily_returns):
+        wins = daily_returns[daily_returns['pnl_absolute']>=0]
+        total_prof = sum(wins['pnl_absolute'])
+        print(total_prof/len(wins))
+        return len(wins)/len(daily_returns)*100
 
+    def winCount(self, daily_returns, i):
+        wins = daily_returns[daily_returns['pnl_absolute']>=0]
+        if i >0:
+            return len(wins)
+        else:
+            return len(daily_returns) - len(wins)
+        
+    def Treturns(self, t):
+        cum_pnl = self.daily_returnts['cum_pnl'].tolist()
+        cum_pnl = cum_pnl[-1*t:]
+        ret = cum_pnl[-1] - cum_pnl[0]
+        print(ret*100/self.initial_investment)
+        return ret, ret*100/self.initial_investment
+    
+    def Last_returns(self, diff):
+        daily_pnl = self.daily_returnts['cum_pnl'].tolist()
+        dayt_amt = daily_pnl[-1]
+        dayt = self.daily_returnts.index[-1]
+        date_format = "%Y-%m-%d"
+        date_obj = datetime.strptime(dayt, date_format)
+        one_year_prior = date_obj - diff
+        value = self.daily_returnts.loc[one_year_prior, 'cum_pnl']
+        ret = dayt_amt - value
+        print(ret*100/self.initial_investment)
+        return ret, ret*100/self.initial_investment   
 
+    def avgReturns(self, daily_returns):
+        daily_returns['returns'] = daily_returns['cum_pnl']/self.initial_investment *100
+        avg_returns = daily_returns['cum_pnl'].mean()
+        avg_returns_pct = daily_returns['returns'].mean()
+        return avg_returns, avg_returns_pct
+    
+    def drawdown(self):
+        self.csv_data['cum_max'] = self.csv_data['equity_curve'].cummax()
+        self.csv_data['drawdown'] = self.csv_data['equity_curve'] - self.csv_data['cum_max']
+        self.csv_data['drawdown_pct'] = (self.csv_data['drawdown']/self.csv_data['cum_max'])*100
+    
+        return self.csv_data['drawdown'].min(), self.csv_data['drawdown_pct'].min()
+    
     def daily_returns_hist(self, daily_returns):
         plt.hist(daily_returns['pnl_absolute'])
         plt.savefig("histogram")
@@ -85,9 +193,9 @@ class StatergyAnalysis:
         plt.show()
 
     def roi(self, monthly_returns):
-        ROI = monthly_returns[['cum P&L']].iloc[-1]
-        ROI_perct = round((ROI/150000)*100,2)
-        return ROI, ROI_perct
+        ROI = monthly_returns[['cum_pnl']].iloc[-1]
+        ROI_perct = round((ROI.values[0]/150000)*100,2)
+        return ROI.values[0], ROI_perct
 
     def num_profit(self, returns):
         return sum(returns['pnl_absolute'] > 0)
@@ -130,7 +238,27 @@ class StatergyAnalysis:
         df.set_index('Value', inplace=True)
         return fig, df
 
-    def HIT(self, prfit, total):
-        return round((prfit/total*100, 1))
-
-
+    def HIT(self):
+        return round((self.num_wins/self.numTrades*100), 1)
+    
+    def num_tradeType(self, quant):
+        i = -1
+        if quant == "short":
+            i = 1
+        elif quant == "long":
+            i = 0
+        else :
+            return None
+        
+        trad = self.csv_data[self.csv_data['entry_transaction_type'] == i]
+        return len(trad)
+            
+    def avgTrades(self, daily_returns):
+        return len(self.csv_data)/len(daily_returns)
+    
+    def ProfitFactor(self):
+        positive_values = self.csv_data[self.csv_data['pnl_absolute'] > 0]
+        neg_values = self.csv_data[self.csv_data['pnl_absolute'] < 0]
+        profit = positive_values['pnl_absolute'].sum()
+        loss = neg_values['pnl_absolute'].sum()
+        return profit/loss
